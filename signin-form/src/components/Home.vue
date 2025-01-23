d6d25005eed4f0d38d7fbbb4be09f56e13f30bca
Hyemin-Youn <hyemin9973@gmail.com>
2025-01-24 03:22:45 +0900

    update

---
+++ signin-form/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Welcome to Home Page</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Home",
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    text-align: center;
+    margin-top: 20px;
+  }
+  </style>
+  
--- signin-form/src/router/index.js
+++ signin-form/src/router/index.js
@@ -1,10 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../components/Login.vue'; // Login 컴포넌트
-import Home from '../components/Home.vue'; // Home 컴포넌트
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/Home.vue";
+import Login from "../components/Login.vue";
 
 const routes = [
-  { path: '/', component: Login },
-  { path: '/home', component: Home },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
 ];
 
 const router = createRouter({
